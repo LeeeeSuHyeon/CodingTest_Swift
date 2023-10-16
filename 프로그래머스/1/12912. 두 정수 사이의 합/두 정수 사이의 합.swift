b8ae2bef79a08b2145e26b3075930b4b4c7db678
LeeeeSuHyeon <105594739+LeeeeSuHyeon@users.noreply.github.com>
2023-10-16 14:56:11 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.25 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.swift
@@ -0,0 +1,13 @@
+func solution(_ a:Int, _ b:Int) -> Int64 {
+    
+    var arr = [Int]()
+    arr.append(a)
+    arr.append(b)
+    arr.sort()
+    var answer : Int64 = 0
+    for i in arr[0]...arr[1]{
+        answer += Int64(i)
+    }
+    
+    return answer
+}
