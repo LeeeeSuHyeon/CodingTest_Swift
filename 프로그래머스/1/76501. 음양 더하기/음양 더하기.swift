19ec0e6b96c1a0869ac6e50687b45800ae397261
LeeeeSuHyeon <105594739+LeeeeSuHyeon@users.noreply.github.com>
2023-10-17 14:25:14 +0900

    [level 1] Title: 음양 더하기, Time: 0.39 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.swift
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.swift
@@ -2,11 +2,13 @@ import Foundation
 
 func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
     
-    var sum = 0
-    for i in 0..<absolutes.count{
-        sum += signs[i] ? absolutes[i] : -absolutes[i]
-    }
+//     var sum = 0
+//     for i in 0..<absolutes.count{
+//         sum += signs[i] ? absolutes[i] : -absolutes[i]
+//     }
     
-    return sum
+//     return sum
+    
+    return (0..<absolutes.count).map{signs[$0] ? absolutes[$0] : -absolutes[$0]}.reduce(0, +)
     
 }
