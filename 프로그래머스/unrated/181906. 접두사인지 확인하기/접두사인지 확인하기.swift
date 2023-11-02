0a240270d2c7cb5dd1e5b1dce195188453936fcf
LeeeeSuHyeon <105594739+LeeeeSuHyeon@users.noreply.github.com>
2023-11-02 23:41:42 +0900

    [level unrated] Title: 접두사인지 확인하기, Time: 0.10 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ my_string:String, _ is_prefix:String) -> Int {
+    var res = [String]()
+    var str = my_string
+    if(my_string.count < is_prefix.count){return 0}
+    for _ in (0..<is_prefix.count){
+        res.append(String(str.removeFirst()))
+    }
+    return res.joined() == is_prefix ? 1 : 0
+}
