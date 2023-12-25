43747686201c6a2ee31d19d576d30c94c9f0f2af
suhyeon lee <105594739+LeeeeSuHyeon@users.noreply.github.com>
2023-12-25 22:45:49 +0900

    [level 2] Title: 최솟값 만들기, Time: 4.58 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+func solution(_ A:[Int], _ B:[Int]) -> Int
+{
+    var ans = 0
+    let a = A.sorted()
+    let b = B.sorted(by : >)
+    
+    for i in 0..<A.count{
+        ans += a[i] * b[i]
+    }
+
+    return ans
+}
