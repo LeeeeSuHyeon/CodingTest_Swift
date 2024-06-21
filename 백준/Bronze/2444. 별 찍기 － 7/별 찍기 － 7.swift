dad056736cf0d3a4aa77b69213740da2022275f5
suhyeon lee <105594739+LeeeeSuHyeon@users.noreply.github.com>
2024-06-21 22:13:36 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 16 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.swift
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.swift
@@ -4,28 +4,19 @@ let input = Int(readLine()!)!
 let r = input * 2 - 1
 
 for i in 1...r {
+    var output = ""
+    var space = ""
     var star = ""
     if i < input {
-       for j in 1...input - i{
-            star += " "
-       }
-       for j in 1...2 * i - 1{
-           star += "*"
-       }
-        print(star)
-    } else if i > input{
-       for j in 1...i - input{
-            star += " "
-       }
-       for j in 1...2 * (r-i) + 1{
-           star += "*"
-       }
-        print(star)
+        space = String(repeating : " ", count : input - i)
+        star = String(repeating : "*", count : i * 2 - 1)
+    } else if i > input {
+        space = String(repeating : " ", count : i - input)
+        star = String(repeating : "*", count : (r - i) * 2 + 1)
     }
-    else{
-        for j in 1...2 * i - 1{
-            star += "*"
-        }
-        print(star)
+    else {
+        star = String(repeating : "*", count : r)
     }
+    print(space + star)
+ 
 }
