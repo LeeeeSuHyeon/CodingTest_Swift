dc37fc6bac7a3bc73611ad68c892d137a98f0ab9
suhyeon lee <105594739+LeeeeSuHyeon@users.noreply.github.com>
2024-06-21 23:17:30 +0900

    [Bronze I] Title: 단어 공부, Time: 116 ms, Memory: 81516 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.swift
+++ 백준/Bronze/1157. 단어 공부/단어 공부.swift
@@ -2,18 +2,17 @@ import Foundation
 let input = String(readLine()!).uppercased()
 var dic = [Character : Int]()
 for i in input {
-    if let a = dic[i] {
-        dic[i] = a + 1
-    }
-    else {
-        dic[i] = 1
-    }
+    dic[i, default : 0] += 1
 }
 let max = dic.values.max()
 var answer = [Character]()
 for key in dic.keys {
     if dic[key] == max {
         answer.append(key)
+        if answer.count > 1 {
+            break
+        }
     }
+
 }
 print(answer.count != 1 ? "?" : answer[0])
