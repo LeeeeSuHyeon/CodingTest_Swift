db0f13a4514ee528a33878e17c92a1641046c0db
suhyeon lee <105594739+LeeeeSuHyeon@users.noreply.github.com>
2024-08-07 00:12:18 +0900

    [Bronze II] Title: 분해합, Time: 484 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.swift
+++ 백준/Bronze/2231. 분해합/분해합.swift
@@ -3,7 +3,7 @@ import Foundation
 let input = Int(String(readLine()!))!
 var found = false
 for i in 1...input {
-    if input == String(i).map{$0}.map{Int(String($0))!}.reduce(i, +) {
+    if input == String(i).map{Int(String($0))!}.reduce(i, +) {
         print(i)
         found = true
         break
