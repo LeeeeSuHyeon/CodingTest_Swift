de12d86885e9a0dbac3d68d01b239ccdbec2fdba
suhyeon lee <105594739+LeeeeSuHyeon@users.noreply.github.com>
2024-08-08 23:26:55 +0900

    [Bronze II] Title: 진법 변환, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.swift
+++ 백준/Bronze/2745. 진법 변환/진법 변환.swift
@@ -1,22 +1,5 @@
 import Foundation
 let input = readLine()!.split(separator : " ")
-let (n, b) = (input[0].map{String($0)}, Int(input[1])!)
-var answer = 0
-var count = n.count - 1
-for i in 0..<n.count {
-    var newI = Int(n[i]) ?? Int(UnicodeScalar(n[i])!.value) - 55
-    if count > 0 {
-       var num = 1
-        for _ in 1...count {
-            num *= b
-        }
-        answer += num * newI
-        count -= 1
-    }
-    else {
-        answer += newI
-    }
-}
-print(answer)
+print(Int(input[0], radix : Int(input[1])!)!)
 
 
