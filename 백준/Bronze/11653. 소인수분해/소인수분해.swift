f505943476fe02f5a5fabee17094c57cb4b87bbd
suhyeon lee <105594739+LeeeeSuHyeon@users.noreply.github.com>
2024-08-25 23:25:45 +0900

    [Bronze I] Title: 소인수분해, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.swift
+++ 백준/Bronze/11653. 소인수분해/소인수분해.swift
@@ -1,13 +1,16 @@
 import Foundation
 
-let input = Int(readLine()!)!
-var num = input
-var i = 2
-while(num > 1) {
-    if num % i == 0 {
-        print(i)
-        num = num / i
-    } else {
-        i += 1
+var num = Int(readLine()!)!
+if num > 1 {
+    for i in 2...num {
+        if num <= 1 {break}
+        if i * i > num {
+            print(num)
+            break
+        }
+        while(num % i == 0) {
+            print(i)
+            num /= i
+        }
     }
 }
