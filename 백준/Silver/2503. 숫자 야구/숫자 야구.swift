9f59a70ab0299974a4dadb1f1a3909da7f682428
suhyeon lee <105594739+LeeeeSuHyeon@users.noreply.github.com>
2024-08-31 23:18:27 +0900

    [Silver III] Title: 숫자 야구, Time: 8 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Silver/2503. 숫자 야구/숫자 야구.swift
+++ 백준/Silver/2503. 숫자 야구/숫자 야구.swift
@@ -1,52 +1,68 @@
-import Foundation 
 let input = Int(readLine()!)!
-var array = [[Int]]()
+var arr = [[Int]]()
 var answer = 0
-
 for _ in 0..<input {
-    let input = readLine()!.split(separator : " ").map{Int($0)!}
-    array.append(input)
+    arr.append(readLine()!.split(separator : " ").map{Int($0)!})
 }
 
-for i in 1...9 {    // 100의 자리
-    for j in 1...9 {    // 10의 자리
-        for k in 1...9 {    // 1의 자리
-            if i == j || j == k || i == k {
-                continue
-            }
-            var count = 0
-            for arr in array {
-                var strikeCount = 0
-                var ballCount = 0
-               let qi = arr[0] / 100        // 100의 자리    
-               let qj = arr[0] % 100 / 10    // 10의 자리
-               let qk = arr[0] % 10          // 1의 자리   
-                if i == qi {
-                    strikeCount += 1
-                } 
-                if j == qj {
-                    strikeCount += 1
-                } 
-                if k == qk {
-                    strikeCount += 1
-                } 
-                if i == qj || i == qk {
-                    ballCount += 1
-                } 
-                if j == qk || j == qi{
-                    ballCount += 1
-                } 
-                if k == qi || k == qj{
-                    ballCount += 1
-                } 
-                if strikeCount == arr[1] && ballCount == arr[2] {
-                    count += 1
-                }
-            }
-            if count == input {
-                answer += 1
-            }
-        }
+func checker(_ count : Int, _ num : Int) -> Bool {
+    var strike = 0
+    var ball = 0
+    let arrNum = arr[count][0]
+    
+    let arrNumH = arrNum / 100
+    let arrNumT = arrNum % 100 / 10
+    let arrNumO = arrNum % 10
+    
+    let targetH = num / 100
+    let targetT = num % 100 / 10
+    let targetO = num % 10
+    
+    if targetH == targetT || targetT == targetO || targetH == targetO {
+        return false
+    }
+    if targetH == 0 || targetO == 0 || targetT == 0 {
+        return false
+    }
+    if arrNumH == targetH {
+        strike += 1
+    }
+    if arrNumT == targetT {
+        strike += 1
+    }
+    if arrNumO == targetO {
+        strike += 1
+    }
+    if targetT == arrNumO || targetT == arrNumH{
+        ball += 1
+    }
+    if targetH == arrNumO || targetH == arrNumT{
+        ball += 1
+    }
+    if targetO == arrNumT || targetO == arrNumH {
+        ball += 1
+    }
+    if strike == arr[count][1] && ball == arr[count][2] {
+        return true
+    }
+    return false
+    
+}
+
+func recur(_ count : Int, _ num : Int) {
+    if count == input {
+        answer += 1
+        recur(0, num + 1)
+        return
+    }
+    if num == 1000 {
+        return
+    }
+    if checker(count, num) {
+        recur(count + 1, num)
+    } else {
+        recur(0, num + 1)
     }
 }
+recur(0, 100)
 print(answer)
