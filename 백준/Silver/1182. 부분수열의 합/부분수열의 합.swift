bf614d3fb86105e08750d39dc2f8e6176b292267
suhyeon lee <105594739+LeeeeSuHyeon@users.noreply.github.com>
2024-09-13 21:50:42 +0900

    [Silver II] Title: 부분수열의 합, Time: 16 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.swift
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.swift
@@ -2,16 +2,16 @@ let input = readLine()!.split(separator: " ").map{Int($0)!}
 let (N, S) = (input[0], input[1])
 let numArr = readLine()!.split(separator: " ").map {Int($0)!}
 var count = 0
-func recur(_ index : Int, _ arr : [Int]) {
+func recur(_ index : Int, _ sum : Int) {
     if index == N {
-        if arr.reduce(0, +) == S && !arr.isEmpty{
+        if sum == S{
             count += 1
         }
         return
     }
-    recur(index + 1, arr + [numArr[index]])
-    recur(index + 1, arr)
+    recur(index + 1, sum + numArr[index])
+    recur(index + 1, sum)
 }
 
-recur(0, [])
-print(count)
+recur(0, 0)
+print(S == 0 ? count - 1 : count)
