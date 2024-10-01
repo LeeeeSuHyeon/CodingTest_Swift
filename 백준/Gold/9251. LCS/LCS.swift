ea3c2c48bd67167590a219db79903d8aba9bcd64
이수현 <105594739+LeeeeSuHyeon@users.noreply.github.com>
2024-10-01 18:00:41 +0900

    [Gold V] Title: LCS, Time: 36 ms, Memory: 76984 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.swift
+++ 백준/Gold/9251. LCS/LCS.swift
@@ -1,14 +1,16 @@
 let str1 = readLine()!.map{$0}
 let str2 = readLine()!.map{$0}
 var dp = Array(repeating: Array(repeating: 0, count: str2.count + 1), count: str1.count + 1)
-
+var lcs = 0
 for i in 1...str1.count {
     for j in 1...str2.count {
         if str1[i - 1] == str2[j - 1] {
             dp[i][j] = dp[i - 1][j - 1] + 1
+            lcs = max(lcs, dp[i][j])
         } else {
             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+            lcs = max(lcs, dp[i][j])
         }
     }
 }
-print(dp.flatMap{$0}.max() ?? 0)
+print(lcs)
