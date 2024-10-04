95046203e58afae126318c1ff2f65a7d5791f62c
이수현 <105594739+LeeeeSuHyeon@users.noreply.github.com>
2024-10-04 17:41:38 +0900

    [Silver II] Title: 예산, Time: 8 ms, Memory: 70020 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.swift
+++ 백준/Silver/2512. 예산/예산.swift
@@ -1,31 +1,26 @@
 let N = Int(readLine()!)!
 let requests = readLine()!.split(separator: " ").map{Int($0)!}
 let M = Int(readLine()!)!
-if requests.reduce(0, +) <= M {
-    print(requests.max() ?? 0)
-}
-else {
-    var s = 1
-    var e = M
 
-    while s <= e {
-        let mid = (s + e) / 2
-        var sum = 0
-        for request in requests {
-            if request > mid {
-                sum += mid
-            }
-            else {
-                sum += request
-            }
-        }
-        if sum <= M {
-            s = mid + 1
+var s = 1
+var e = requests.max() ?? 0
+
+while s <= e {
+    let mid = (s + e) / 2
+    var sum = 0
+    for request in requests {
+        if request > mid {
+            sum += mid
         }
         else {
-            e = mid - 1
+            sum += request
         }
     }
-    print(e)
-
+    if sum <= M {
+        s = mid + 1
+    }
+    else {
+        e = mid - 1
+    }
 }
+print(e)
