fa9baa0b5a8ded61753f7d26d677671355a5b6da
이수현 <105594739+LeeeeSuHyeon@users.noreply.github.com>
2024-10-06 23:08:13 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 84 ms, Memory: 71648 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.swift
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.swift
@@ -2,21 +2,20 @@ let T = Int(readLine()!)!
 for _ in 0..<T {
     let input = readLine()!.split(separator: " ").map{Int($0)!}
     let (N, M) = (input[0], input[1])
-    let A = readLine()!.split(separator: " ").map{Int($0)!}.sorted(by: >)
-    let B = readLine()!.split(separator: " ").map{Int($0)!}.sorted(by: >)
-
+    let A = readLine()!.split(separator: " ").map{Int($0)!}.sorted()
+    let B = readLine()!.split(separator: " ").map{Int($0)!}.sorted()
+    
+    var aStart = 0
+    var bStart = 0
     var count = 0
-    for i in 0..<N {
-        var startB = 0
-        for j in startB..<M {
-            if A[i] > B[j] {
-                count += 1
-            } else {
-                startB += 1
-            }
+    
+    while aStart < N && bStart < M {
+        if A[aStart] > B[bStart] {
+            count += N - aStart
+            bStart += 1
+        } else {
+            aStart += 1
         }
     }
     print(count)
-
 }
-
