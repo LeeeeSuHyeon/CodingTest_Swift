a6685abb194a312ec0e6f27c623d67e5b0243379
이수현 <105594739+LeeeeSuHyeon@users.noreply.github.com>
2024-12-13 00:20:06 +0900

    [Gold IV] Title: 오큰수, Time: 716 ms, Memory: 176916 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.swift
@@ -0,0 +1,14 @@
+let N = Int(readLine()!)!
+let A = readLine()!.split(separator: " ").map{Int($0)!}
+var answer = Array(repeating: -1, count: N)
+var stack = [[Int]]()
+
+for (index, num) in A.enumerated() {
+    while let last = stack.last, last[0] < num  {
+        answer[last[1]] = num
+        stack.removeLast()
+    }
+    stack.append([num, index])
+}
+
+print(answer.map{String($0)}.joined(separator: " "))
