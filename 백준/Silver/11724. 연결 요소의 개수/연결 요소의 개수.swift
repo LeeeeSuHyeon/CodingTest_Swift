ce338902d26939eb218a5128c8359918e19126d4
이수현 <105594739+LeeeeSuHyeon@users.noreply.github.com>
2024-12-15 19:19:41 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 540 ms, Memory: 78492 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.swift
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.swift
@@ -2,7 +2,7 @@ let NM = readLine()!.split(separator: " ").map{Int($0)!}
 let (N, M) = (NM[0], NM[1])
 var graph = Array(repeating: [Int](), count: N + 1)
 var visited = Array(repeating: false, count: N + 1)
-var arr = [Int]()
+var arr = graph[1]
 var count = 0
 
 for _ in 0..<M {
@@ -12,31 +12,21 @@ for _ in 0..<M {
     graph[v].append(u)
 }
 
-func recur() {
-    if arr.isEmpty {
-        count += 1
-        if visited.filter({$0 == false}).isEmpty {
-            return
-        } else {
-            if let index = visited.firstIndex(of: false) {
-                visited[index] = true
-                arr += graph[index]
-                recur()
-            }
+func dfs(node : Int) {
+    for next in graph[node] {
+        if !visited[next] {
+            visited[next] = true
+            dfs(node: next)
         }
     }
-    
-    if let next = arr.popLast() {
-        visited[next] = true
-        let nextArr = graph[next].filter({visited[$0] == false})
-        arr += nextArr
-        recur()
-    }
-
 }
-visited[0] = true
-visited[1] = true
-arr += graph[1]
-recur()
-print(count)
 
+for i in 1...N{
+    if !visited[i] {
+        count += 1
+        visited[i] = true
+        dfs(node: i)
+    }
+}
+
+print(count)
