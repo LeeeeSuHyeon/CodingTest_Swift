a6a0cc38100ffc11c53c94ff6a6e55154f24c5df
이수현 <105594739+LeeeeSuHyeon@users.noreply.github.com>
2025-01-06 16:28:15 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 200 ms, Memory: 72240 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.swift
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.swift
@@ -1,20 +1,19 @@
 let N = Int(readLine()!)!
-var arr = [[Int]]()
+var arr = [(Int, Int)]()
 
 for _ in 0..<N {
     let xy = readLine()!.split(separator: " ").map{Int($0)!}
     let (x, y) = (xy[0], xy[1])
-    arr.append([x, y])
+    arr.append((x, y))
 }
 
-let sortedArr = arr.sorted(by: {
-    if $0[0] == $1[0] {
-        return $0[1] < $1[1]
+arr.sort { first, second in
+    if first.0 == second.0 {
+        return first.1 < second.1
     } else {
-        return $0[0] < $1[0]
+        return first.0 < second.0
     }
-})
-
-for answer in sortedArr {
-    print("\(answer[0]) \(answer[1])")
+}
+for answer in arr {
+    print("\(answer.0) \(answer.1)")
 }
