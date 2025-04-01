8505fd732de68bcbc727d2b90cb36f824c9a0251
LeeSuHyeon <105594739+LeeeeSuHyeon@users.noreply.github.com>
2025-04-01 20:27:34 +0900

    [level 3] Title: 숫자 게임, Time: 383.64 ms, Memory: 25 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.swift
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.swift
@@ -3,20 +3,10 @@ import Foundation
 func solution(_ a:[Int], _ b:[Int]) -> Int {
     var sortedA = a.sorted()
     var sortedB = b.sorted()
-    var answer = 0
+    var index = 0
     
-    while !sortedA.isEmpty {
-        let A = sortedA.last!
-        let B = sortedB.last!
-        
-        if A < B { 
-            answer += 1
-            sortedA.removeLast()
-            sortedB.removeLast()
-        } else {
-            sortedA.removeLast()
-            sortedB.removeFirst()
-        }
-    }
-    return answer
+   for b in sortedB where b > sortedA[index] {
+       index += 1
+   }
+    return index
 }
