2c5a4ae9cc10d9ffc757094b14dc2d415a44c36b
LeeSuHyeon <105594739+LeeeeSuHyeon@users.noreply.github.com>
2025-09-12 11:21:40 +0900

    [level 3] Title: 네트워크, Time: 6.39 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.swift
+++ 프로그래머스/3/43162. 네트워크/네트워크.swift
@@ -5,12 +5,13 @@ func solution(_ n:Int, _ computers:[[Int]]) -> Int {
     var count = 0 // 네트워크 개수
     var queue = [0] // 연결되어 있는 컴퓨터의 인덱스 저장 (첫 번째 컴퓨터부터 방문)
     
-    while true { // visited가 모두 true일 때까지(모든 컴퓨터 방문할 때까지)
-        if let computer = queue.popLast() { // 큐에서 방문할 노드 제거
+    while true { // 모든 컴퓨터 방문할 때까지 반복
+        if let computer = queue.popLast() { // 큐에서 방문할 컴퓨터 제거
             visited[computer] = true // 방문 처리
-            for (i, connect) in computers[computer].enumerated() {
-                if i == computer || visited[i] || connect == 0 || queue.contains(i) { continue }
-                queue.append(i)
+            for (i, connect) in computers[computer].enumerated() { // 방문한 컴퓨터와 연결된 컴퓨터 탐색
+			        // 자신, 방문했던 컴퓨터, 연결되지 않은 컴퓨터 제외 
+                if i == computer || visited[i] || connect == 0 { continue }
+                queue.append(i) // 큐에 추가 
             }
         } else { // 비어있으면
             count += 1 // 네트워크 1 증가
@@ -21,5 +22,6 @@ func solution(_ n:Int, _ computers:[[Int]]) -> Int {
             }
         }
     }
+    
     return count
 }
