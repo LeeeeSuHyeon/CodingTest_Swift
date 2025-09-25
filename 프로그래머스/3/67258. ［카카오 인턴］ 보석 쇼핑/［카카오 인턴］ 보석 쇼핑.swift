648eb69dfb021581c60321baf733fa0536713fc0
LeeSuHyeon <105594739+LeeeeSuHyeon@users.noreply.github.com>
2025-09-25 12:25:21 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 89.44 ms, Memory: 24.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+func solution(_ gems:[String]) -> [Int] {
+    let gemsKind = Set(gems)
+    var gemsCount = [String: Int]()
+    var answer = [0, 100_000]
+    var left = 0
+    var right = 0
+    
+    while right <= gems.count, left <= right {
+        
+        if gemsCount.values.count == gemsKind.count {
+            if answer[1] - answer[0] > right - left {
+                answer = [left, right]
+            }
+            
+            gemsCount[gems[left]]! -= 1
+            if gemsCount[gems[left]] == 0 { gemsCount[gems[left]] = nil }
+            left += 1
+        } else {
+            if right == gems.count { break }
+            gemsCount[gems[right], default: 0] += 1
+            right += 1
+        }
+
+    }
+    return [answer[0] + 1, answer[1]]
+}
+
+/* 1110
+
+1. 투포인터 이용
+2. 구간의 보석들이 모두 포함되면 left += 1
+3. 보석이 포함되지 않으면 right += 1
+4. 포함 될 떄마다 구간의 길이, left, right 구함
+
+*/
